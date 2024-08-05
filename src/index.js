89f1d8084ff27916df0dc063e5edd629677652c0
蒋良子良 <qzljiang@isoftstone.com>
2024-08-05 15:05:56 +0800

    change domain to remote-cat

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.remote-cat.cn": dockerHub,
+  "quay.remote-cat.cn": "https://quay.io",
+  "gcr.remote-cat.cn": "https://gcr.io",
+  "k8s-gcr.remote-cat.cn": "https://k8s.gcr.io",
+  "k8s.remote-cat.cn": "https://registry.k8s.io",
+  "ghcr.remote-cat.cn": "https://ghcr.io",
+  "cloudsmith.remote-cat.cn": "https://docker.cloudsmith.io",
+  "ecr.remote-cat.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.remote-cat.cn": dockerHub,
 };
 
 function routeByHosts(host) {
